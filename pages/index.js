a86540b8eb7592658eae66cf2102367982435eda
ErerexIDChx <luwibuu100@gmail.com>
2025-02-12 20:25:24 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import { Analytics } from "@vercel/analytics/react";
 import swaggerConfig from "./swagger-config.json";
 import { Inter } from "next/font/google";
 import { useEffect } from "react";
-import { tailwindCSS }from "tailwindcss";
+import { tailwindCSS } from "tailwindcss";
 
 const inter = Inter({ subsets: ["latin"] });
 
