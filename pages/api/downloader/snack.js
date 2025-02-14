9fe4f171bf918302f4df3ef48862da9517f2b8e2
ErerexID Chx <luwibuu100@gmail.com>
2025-02-14 08:27:47 +0700

    Update snack.js

--- pages/api/downloader/snack.js
+++ pages/api/downloader/snack.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import cheerio from "cherioo";
+import cheerio from "cheerio";
 import { API_KEY, CREATOR } from "../../../settings";
 
 export default async function handler(req, res) {
@@ -12,9 +12,9 @@ export default async function handler(req, res) {
     }
 
     const { url } = req.query;
-    
+
     try {
-        const data await downloadSnackVideo(url);
+        const data = await downloadSnackVideo(url);
         res.status(200).json({
             status: true,
             creator: CREATOR,
@@ -31,27 +31,25 @@ export default async function handler(req, res) {
 }
 
 async function downloadSnackVideo(url) {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const response = await axios.get(url);
-            const $ = cheerio.load(response.data);
-            let result = {
-                metadata: {},
-                download: null
-            };
-            const json = JSON.parse($("#VideoObject").text().trim());
-            result.metadata.title = json.name;
-            result.metadata.thumbnail = json.thumbnailUrl[0];
-            result.metadata.uploaded = new Date(json.uploadDate).toLocaleString();
-            result.metadata.comment = json.commentCount;
-            result.metadata.watch = json.interactionStatistic[0].userInteractionCount;
-            result.metadata.likes = json.interactionStatistic[1].userInteractionCount;
-            result.metadata.share = json.interactionStatistic[2].userInteractionCount;
-            result.metadata.author = json.creator.mainEntity.name;
-            result.download = json.contentUrl;
-            resolve(result);
-        } catch (error) {
-            reject({ msg: error.message });
-        }
-    });
+    try {
+        const response = await axios.get(url);
+        const $ = cheerio.load(response.data);
+        let result = {
+            metadata: {},
+            download: null
+        };
+        const json = JSON.parse($("#VideoObject").text().trim());
+        result.metadata.title = json.name;
+        result.metadata.thumbnail = json.thumbnailUrl[0];
+        result.metadata.uploaded = new Date(json.uploadDate).toLocaleString();
+        result.metadata.comment = json.commentCount;
+        result.metadata.watch = json.interactionStatistic[0].userInteractionCount;
+        result.metadata.likes = json.interactionStatistic[1].userInteractionCount;
+        result.metadata.share = json.interactionStatistic[2].userInteractionCount;
+        result.metadata.author = json.creator.mainEntity.name;
+        result.download = json.contentUrl;
+        return result;
+    } catch (error) {
+        throw new Error(error.message);
+    }
 }
