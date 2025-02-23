52729aba49e2c0625821b0417f7215d2e2a088b8
Sanzy <124222684+sanzydev@users.noreply.github.com>
2025-02-23 08:27:23 +0700

    Update hello.js

--- src/app/api/hello.js
+++ src/app/api/hello.js
@@ -1,1 +1,13 @@
-export default function handler(req, res) { if (req.method === 'GET') { res.status(200).json({ message: 'Halo, ini API Velyn!' }); } else if (req.method === 'POST') { const { name } = req.body; res.status(200).json({ message: Halo, ${name}! }); } else { res.setHeader('Allow', ['GET', 'POST']); res.status(405).end(Method ${req.method} Tidak Diizinkan); } }
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    res.status(200).json({ message: 'Halo, ini API Velyn!' });
+  } else if (req.method === 'POST') {
+    const { name } = req.body;
+    res.status(200).json({ message: `Halo!` });
+  } else {
+    res.setHeader('Allow', ['GET', 'POST']);
+    res.status(405).end(`Method ${req.method} Tidak Diizinkan`);
+  }
+}
